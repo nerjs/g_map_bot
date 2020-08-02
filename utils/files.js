c922caffc8ecdefdfea414d447921e78df51a1fe
nerjs <nerjs.stap@gmail.com>
2020-08-03 00:21:19 +0300

    fix files & lint

--- utils/files.js
+++ utils/files.js
@@ -6,7 +6,7 @@ const filesDir = path.join(__dirname, 'files')
 
 module.exports = fs
   .ensureDir(filesDir)
-  .then(() => fs.emptyDir())
+  .then(() => fs.emptyDir(filesDir))
   .then(() => fs.writeFile(path.join(filesDir, 'status'), 'OK'))
   .then(() => logger.info('Files ready'))
   .catch(e => {
