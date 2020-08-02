ad791a18a33ff664efffa4692fe71fba96285eae
nerjs <nerjs.stap@gmail.com>
2020-08-03 01:52:21 +0300

    fix files

--- utils/files.js
+++ utils/files.js
@@ -3,11 +3,14 @@ const fs = require('fs-extra')
 const logger = require('nlogs')(module)
 
 const filesDir = path.join(__dirname, 'files')
+logger.debug(path.join(filesDir, 'status'))
 
 module.exports = fs
   .ensureDir(filesDir)
   .then(() => fs.emptyDir(filesDir))
   .then(() => fs.writeFile(path.join(filesDir, 'status'), 'OK'))
+  .then(() => logger.info('Status file created'))
+  .then(() => fs.stat(path.join(filesDir, 'status')))
   .then(() => logger.info('Files ready'))
   .catch(e => {
     logger.error(e)
